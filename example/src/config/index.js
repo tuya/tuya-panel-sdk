89fffabf3c9c1c172f48eee01875dc01f59b5084
youngjuning <youngjuning@aliyun.com>
2020-11-20 18:16:50 +0800

    w i p ( e x a m p l e ) :   迁 移   e x a m p l e

---
+++ example/src/config/index.js
@@ -0,0 +1,21 @@
+export const SCOPE_MAP = {
+  // 'tuya-panel-sdk': 'Basic',
+  // 'tuya-panel-animation-sdk': 'Animation',
+  // 'tuya-panel-cbt-sdk': 'Cbt',
+  // 'tuya-panel-cooker-sdk': 'Cooker',
+  // 'tuya-panel-electrician-sdk': 'Electrician',
+  // 'tuya-panel-fullroom-sdk': 'FullRoom',
+  // 'tuya-panel-gateway-sdk': 'Gateway',
+  // 'tuya-panel-health-sdk': 'Health',
+  // 'tuya-panel-ipc-sdk': 'Ipc',
+  // 'tuya-panel-lamp-sdk': 'Lamp',
+  // 'tuya-panel-remote-sdk': 'Remote',
+  // 'tuya-panel-robot-sdk': 'SweepRobot',
+  // 'tuya-panel-sensor-sdk': 'Sensor',
+  // 'tuya-panel-standard-sdk': 'Standard',
+  // 'tuya-panel-lock-sdk': 'Lock',
+};
+
+export { default as routers } from './routers';
+
+export { default as sections } from './sections';
