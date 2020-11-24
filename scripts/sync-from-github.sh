389b1c3723bb54edb2b5fe5b13306c66900dea37
youngjuning <youngjuning@aliyun.com>
2020-11-24 19:29:36 +0800

    chore: edit ci

--- scripts/sync-from-github.sh
+++ scripts/sync-from-github.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 origin=$(git remote get-url origin)
-if [ $origin != 'https://github.com/tuya/tuya-panel-sdk.git' ]
+branch=$(git branch | grep "*")
+if [[ $origin = 'https://registry.code.tuya-inc.top/TuyaRN/tuya-native-elements.git' && $branch = 'sync' ]]
 then
   git remote add github https://github.com/tuya/tuya-panel-sdk.git
   git fetch github main
   git merge github/main
   git remote rm github
 else
-  echo -e "\033[31mERROR: 该命令只能在内部仓库执行\033[0m"
+  echo -e "\033[31mERROR:  请在内部仓库的 sync 分支执行该脚本\033[0m"
   exit 1
 fi
