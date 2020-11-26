49b54c6751167e55c79b47c6c397f1d9cc42e1cc
youngjuning <youngjuning@aliyun.com>
2020-11-26 19:49:05 +0800

    chore: sync gitlab to github

---
+++ example/src/config/theme.js
@@ -0,0 +1,47 @@
+export const themeLight = {
+  fontColor: '#333', // 主要字体色
+  titleBg: '#fff', // 头部栏底色
+  titleFontColor: '#333', // 头部栏字体颜色
+  boardBg: '#f8f8f8', // 整体底色
+  cellBg: '#fff', // 列表底色
+  cellLine: 'rgba(51, 51, 51, 0.1)', // 列表分隔栏颜色
+  subFontColor: 'rgba(51, 51, 51, 0.5)', // 副标题字体颜色
+  btnBg: '#fff', // 添加定时按钮底色
+  btnBorder: 'rgba(51, 51, 51, 0.2)', // 添加定时按钮边框色
+  btnFontColor: 'rgba(51, 51, 51, 0.8)', // 添加定时按钮字体颜色
+  tintColor: '#e5e5e5', // 关闭情况下背景色，不开放
+  onTintColor: '#4CD964', // 开启情况下背景色
+  thumbTintColor: '#fff', // 关闭情况下滑块背景色
+  onThumbTintColor: '#fff', // 开启情况下滑块背景色
+  ios_backgroundColor: '#f0f0f0',
+  repeatColor: '#44DB5E',
+  statusBgStyle: 'default', // 状态栏字体颜色
+};
+
+export const themeDark = {
+  titleBg: '#2b2c2a', // 头部栏底色
+  titleFontColor: '#fff', // 头部栏字体颜色
+  boardBg: '#2b2c2a', // 整体底色
+  fontColor: '#fff', // 主要字体色
+  cellBg: 'rgba(255, 255, 255, 0.02)', // 列表底色
+  cellLine: 'rgba(255, 255, 255, 0.1)', // 列表分隔栏颜色
+  subFontColor: 'rgba(255, 255, 255, 0.5)', // 副标题字体颜色
+  btnBg: '#fff', // 添加定时按钮底色
+  btnBorder: 'transparent', // 添加定时按钮边框色
+  btnFontColor: '#2b2c2a', // 添加定时按钮字体颜色
+  tintColor: '#e5e5e5', // 关闭情况下背景色，不开放
+  onTintColor: '#4CD964', // 开启情况下背景色
+  thumbTintColor: '#fff', // 关闭情况下滑块背景色
+  onThumbTintColor: '#fff', // 开启情况下滑块背景色
+  ios_backgroundColor: 'rgba(255, 255, 255, 0.16)',
+  repeatColor: '#4A90E2',
+  statusBgStyle: 'default', // 状态栏字体颜色
+};
+
+export default {
+  // timer: themeLight,
+  // timer: themeDark,
+  // popup: {
+  //   type: 'dark',
+  // },
+};
