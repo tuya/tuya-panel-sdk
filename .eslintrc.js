ee8d3922ee17f29d750b3d0ff2a96d55a68c7d08
youngjuning <youngjuning@aliyun.com>
2020-11-28 15:35:13 +0800

    chore: remove tuya.m from literal-blacklist

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,6 @@ module.exports = {
     'literal-blacklist/literal-blacklist': [2, [
       'tuya.ai',
       'tuya.ia',
-      'tuya.m',
       'tuya.industry',
       'tuya.smarthome',
       '@tuya-rn'
