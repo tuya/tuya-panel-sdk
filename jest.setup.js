e2f579eebdb40b54d705f1134a01af62cf9d5fa2
youngjuning <youngjuning@aliyun.com>
2020-12-02 12:00:15 +0800

    chore: sync gitlab to github

---
+++ jest.setup.js
@@ -0,0 +1,7 @@
+import 'react-native';
+import 'react-native-mock-render/mock';
+import 'react-native/Libraries/Animated/src/bezier'; // for https://github.com/facebook/jest/issues/4710
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
