6136c7e4175cdd2dddf4ffa536ee20a057842ed2
dy.li <dy.li@tuya.com>
2021-03-03 12:04:32 +0800

    feat(feature/tuya-panel-robot-sdk): api&map

--- packages/tuya-panel-robot-sdk/src/components/index.ts
+++ packages/tuya-panel-robot-sdk/src/components/index.ts
@@ -1,7 +1,2 @@
 export { default as RCTGyroMap, TYRCTGyroMapManager } from './Basic/RCTGyroMap';
 export { default as RCTLaserMap, TYLaserManager, TYRCTTypeMapManager } from './Basic/RCTLaserMap';
-
-export { default as MultiFaultDetail } from './RobotBusiness/MultiFaultDetail';
-export { default as MultiFaultToast } from './RobotBusiness/MultiFaultToast';
-export { EmptyMap, DrawMap } from './RobotBusiness/EmptyMap';
-export { default as Loading } from './RobotBusiness/Loading';
