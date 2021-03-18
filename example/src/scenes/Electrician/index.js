c441fbf7d6ab0757bfb85ae327ae093c1db8adf2
hujj <hujj@tuya.com>
2021-03-18 15:56:21 +0800

    f e a t :   a d d   e l e c t r i c i a n

--- example/src/scenes/Electrician/index.js
+++ example/src/scenes/Electrician/index.js
@@ -5,12 +5,36 @@ import { subRouters } from '../../config/routers';
 import CountdownElectricianScene from './Countdown';
 import SocketElectricianScene from './Socket';
 import CountdownListElectricianScene from './CountdownList';
-
+import CurtainElectricianScene from './Curtain';
+import AnimatedHeaderElectricianScene from './AnimatedHeader';
+import CurtainControlElectricianScene from './CurtainControl';
+import DropDownElectricianScene from './dropDown';
+import EleSettingElectricianScene from './EleSetting';
+import MagicLayoutElectricianScene from './MagicLayout';
+import NameEditorElectricianScene from './NameEditor';
+import PowerLineElectricianScene from './PowerLine';
+import PushAnimateElectricianScene from './PushAnimate';
+import ScheduleListElectricianScene from './ScheduleList';
+import ShuffingListElectricianScene from './ShuffingList';
+import SlideLayoutElectricianScene from './SlideLayout';
+import SocketListItemElectricianScene from './SocketListItem';
 export default class AnimatedScene extends Component {
   static Countdown = CountdownElectricianScene;
   static Socket = SocketElectricianScene;
   static CountdownList = CountdownListElectricianScene;
-
+  static Curtain = CurtainElectricianScene;
+  static AnimatedHeader = AnimatedHeaderElectricianScene;
+  static CurtainControl = CurtainControlElectricianScene;
+  static DropDown = DropDownElectricianScene;
+  static EleSetting = EleSettingElectricianScene;
+  static MagicLayout = MagicLayoutElectricianScene;
+  static NameEditor = NameEditorElectricianScene;
+  static PowerLine = PowerLineElectricianScene;
+  static PushAnimate = PushAnimateElectricianScene;
+  static ScheduleList = ScheduleListElectricianScene;
+  static ShuffingList = ShuffingListElectricianScene;
+  static SlideLayout = SlideLayoutElectricianScene;
+  static SocketListItem = SocketListItemElectricianScene;
   get data() {
     return produceRouterDatas(subRouters.filter(r => /^Electrician.+\w*$/.test(r.id)));
   }
