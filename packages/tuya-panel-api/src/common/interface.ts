325feb9edd96e33eba3f125173264ea32b754cf4
Miss Hu <32921020+JulyAnnes@users.noreply.github.com>
2021-04-20 18:34:48 +0800

    refactor: move linkage to common (#27)
    Co-authored-by: hujj

--- packages/tuya-panel-api/src/common/interface.ts
+++ packages/tuya-panel-api/src/common/interface.ts
@@ -333,3 +333,30 @@ export interface IAlarmSdk {
   getDevAlarmList;
   setAlarmSwitch;
 }
+
+// 联动相关
+export interface IGetLinkageDeviceList {
+  gid: number;
+  sourceType: string;
+}
+
+export interface IBindRule {
+  associativeEntityId: string;
+  ruleId: string;
+  entitySubIds: string;
+  expr: [string[]];
+  bizDomain: string;
+}
+
+export interface IGetBindRuleList {
+  bizDomain: string;
+  sourceEntityId: string;
+  entityType: number;
+}
+
+export interface IRemoveRule {
+  bizDomain: string;
+  sourceEntityId: string;
+  associativeEntityId: string;
+  associativeEntityValue: string;
+}
--- packages/tuya-panel-api/src/index.ts
+++ packages/tuya-panel-api/src/index.ts
@@ -1,7 +1,5 @@
 import common from './common';
 import robot from './robot';
-import electrician from './electrician';
 
 export const commonApi = common;
 export const robotApi = robot;
-export const electricianApi = electrician;
