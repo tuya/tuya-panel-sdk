8d81b7b77040aa009c1220a6e45907f057ad7643
浮丘 <35797679+zegezs@users.noreply.github.com>
2021-06-17 17:18:03 +0800

    fix: defend-time i18n (#40)

---
+++ packages/tuya-panel-lock-sdk/src/defend-time/i18n/string.ts
@@ -0,0 +1,14 @@
+export default {
+  en: {
+    safeTip: 'Safe Guarding for',
+    day: 'days',
+    random: 'random',
+    passwordInput: 'enter password',
+  },
+  zh: {
+    safeTip: '已安全守护',
+    day: '天',
+    random: 'random',
+    passwordInput: 'enter password',
+  },
+};
