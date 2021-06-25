b53dcb8c499e2d27f32871a1e6288ba030508d3c
Konpaku Youmu <saber2pr@gmail.com>
2021-06-25 16:21:12 +0800

    feat(interface): add return type (#34)

--- packages/tuya-panel-api/src/common/scene.ts
+++ packages/tuya-panel-api/src/common/scene.ts
@@ -5,6 +5,7 @@ import { Scene, Automation, ISelectedScene, IBindScene } from './interface';
 /**
  * 获取当前设备已绑定的所有场景
  * @param {string} devId 设备Id
+ * @docs https://developer.tuya.com/cn/docs/iot/scene-related-interface?id=K9pouo6v4lf7v#title-0-%E8%8E%B7%E5%8F%96%E8%AE%BE%E5%A4%87%E7%BB%91%E5%AE%9A%E5%9C%BA%E6%99%AF
  */
 const getAllBindScenes = (devId: string): Promise<Scene[]> => {
   return TYSdk.apiRequest('tuya.m.linkage.rule.bind.wifi.query', { devId });
@@ -13,6 +14,7 @@ const getAllBindScenes = (devId: string): Promise<Scene[]> => {
 /**
  * 获取当前设备创建的所有场景
  * @param {string} devId 设备Id
+ * @docs https://developer.tuya.com/cn/docs/iot/scene-related-interface?id=K9pouo6v4lf7v#title-1-%E8%8E%B7%E5%8F%96%E8%AE%BE%E5%A4%87%E5%88%9B%E5%BB%BA%E5%9C%BA%E6%99%AF
  */
 const getAllCreatedScenes = (devId: string): Promise<Automation[]> => {
   return TYSdk.apiRequest('tuya.m.linkage.rule.brief.query', { devId });
@@ -25,6 +27,7 @@ const getAllCreatedScenes = (devId: string): Promise<Automation[]> => {
  * @param {string} dpValue dpId 对应的值
  * @param {string} btnId 按键
  * @param {string} ruleId 规则 Id
+ * @docs https://developer.tuya.com/cn/docs/iot/scene-related-interface?id=K9pouo6v4lf7v#title-2-%E7%BB%91%E5%AE%9A%E5%9C%BA%E6%99%AF
  */
 const bindSelectedScene = (params: ISelectedScene): Promise<boolean> => {
   return TYSdk.apiRequest('tuya.m.linkage.rule.bind.wifi.save', { ...params });
@@ -34,6 +37,7 @@ const bindSelectedScene = (params: ISelectedScene): Promise<boolean> => {
  * 解绑场景
  * @param {string} btnId 按键
  * @param {string} devId 设备 Id
+ * @docs https://developer.tuya.com/cn/docs/iot/scene-related-interface?id=K9pouo6v4lf7v#title-3-%E8%A7%A3%E7%BB%91%E5%9C%BA%E6%99%AF
  */
 const unBindScene = (params: IBindScene): Promise<boolean> => {
   return TYSdk.apiRequest('tuya.m.linkage.rule.bind.wifi.remove', { ...params });
@@ -42,6 +46,7 @@ const unBindScene = (params: IBindScene): Promise<boolean> => {
 /**
  * 触发场景
  * @param {string} ruleId 场景规则 ID
+ * @docs https://developer.tuya.com/cn/docs/iot/scene-related-interface?id=K9pouo6v4lf7v#title-4-%E8%A7%A6%E5%8F%91%E5%9C%BA%E6%99%AF
  */
 const triggerScene = (ruleId: string): Promise<boolean> => {
   return TYSdk.apiRequest('tuya.m.linkage.rule.trigger', { ruleId });
