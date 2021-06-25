17640cff5cdcf6b623353c3575ec5eb644fd1ca2
Joey <Joeynaive@foxmail.com>
2021-06-25 18:37:34 +0800

    feat(ipc): add ty-ipc-ptz component
    * feat(ipc): ty-ipc-ptz
    
    * feat:tyIpcPtz
    
    * fix: eslint fix
    
    * fix: modify style
    
    * feat: new component
    
    * feat(ipc): new component
    
    * feat(ipc): new component
    
    * feat(ipc): new component
    
    * feat(ipc): new component
    
    * chore: add ; to example/src/scenes/Ipc/index.js
    
    * chore: add ; to example/src/scenes/Ipc/index.js
    
    * chore: add ; to example/src/scenes/Ipc/index.js
    
    Co-authored-by: 洛竹 <youngjuning@aliyun.com>

---
+++ packages/tuya-panel-ipc-sdk/src/ty-ipc-ptz/style/index.tsx
@@ -0,0 +1,45 @@
+import { StyleSheet } from 'react-native';
+
+const Styles = StyleSheet.create({
+  PtzCommonPage: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  circleImage: {
+    height: 50,
+    width: 50,
+  },
+  dotImage: {
+    height: 10,
+    tintColor: '#fc2f07',
+    width: 10,
+  },
+  hoverImage: {
+    alignItems: 'center',
+    bottom: 0,
+    justifyContent: 'center',
+    left: 0,
+    position: 'absolute',
+    right: 0,
+    top: 0,
+  },
+  pieBox: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  pieCommon: {
+    backgroundColor: 'transparent',
+  },
+  ptzDotImage: {
+    height: 10,
+    left: '50%',
+    marginLeft: -5,
+    marginTop: -5,
+    position: 'absolute',
+    top: '50%',
+    width: 10,
+  },
+});
+
+export default Styles;
