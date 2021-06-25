542590c1dbd8a3870235ac385da0ca056c1f7403
Joey <Joeynaive@foxmail.com>
2021-06-25 18:37:34 +0800

    f e a t ( i p c ) :   a d d   t y - i p c - p t z   p o n e n t
      *   f e a t ( i p c ) :   t y - i p c - p t z  
       
      *   f e a t : t y I p c P t z  
       
      *   f i x :   e s l i n t   f i x  
       
      *   f i x :   m o d i f y   s t y l e  
       
      *   f e a t :   n e w   p o n e n t  
       
      *   f e a t ( i p c ) :   n e w   p o n e n t  
       
      *   f e a t ( i p c ) :   n e w   p o n e n t  
       
      *   f e a t ( i p c ) :   n e w   p o n e n t  
       
      *   f e a t ( i p c ) :   n e w   p o n e n t  
       
      *   c h o r e :   a d d   ;   t o   e x a m p l e / s r c / s c e n e s / I p c / i n d e x . j s  
       
      *   c h o r e :   a d d   ;   t o   e x a m p l e / s r c / s c e n e s / I p c / i n d e x . j s  
       
      *   c h o r e :   a d d   ;   t o   e x a m p l e / s r c / s c e n e s / I p c / i n d e x . j s  
       
      C o - a u t h o r e d - b y :   洛 竹   < y o u n g j u n i n g @ a l i y u n   >

---
+++ packages/tuya-panel-ipc-sdk/src/ty-ipc-ptz/config/index.ts
@@ -0,0 +1,44 @@
+import { Dimensions, Platform } from 'react-native';
+
+import { Utils } from 'tuya-panel-kit';
+
+const {
+  convertX: cx,
+  convertY: cy,
+  convert,
+  isIphoneX,
+  statusBarHeight,
+  topBarHeight,
+} = Utils.RatioUtils;
+
+const { width: winWidth, height: winHeight } = Dimensions.get('screen');
+
+const smallScreen: boolean = winHeight < 667;
+
+const middlleScreen: boolean = winHeight >= 667 && winHeight <= 736;
+
+const isIOS: boolean = Platform.OS === 'ios';
+
+// 针对7p做单独处理
+const is7Plus = winHeight === 736 && isIOS;
+
+// 普通播放器高度
+const normalPlayerWidth: number = Math.ceil(winWidth);
+const normalPlayerHeight: number = Math.ceil((winWidth * 9) / 16);
+
+export default {
+  cx,
+  cy,
+  convert,
+  isIphoneX,
+  winWidth,
+  winHeight,
+  topBarHeight,
+  smallScreen,
+  middlleScreen,
+  statusBarHeight,
+  normalPlayerWidth,
+  normalPlayerHeight,
+  isIOS,
+  is7Plus,
+};
