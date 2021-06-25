8d36c01bacbf7a3ca11ec1c8f5e82980380ef1d0
Konpaku Youmu <saber2pr@gmail.com>
2021-06-25 16:21:12 +0800

    f e a t ( i n t e r f a c e ) :   a d d   r e t u r n   t y p e   ( # 3 4 )

--- packages/tuya-panel-api/src/robot/ossDataApi.ts
+++ packages/tuya-panel-api/src/robot/ossDataApi.ts
@@ -7,6 +7,7 @@ const TYLaserManager = NativeModules.TYRCTLaserManager;
  *
  * @param {String} [devId=TYSdk.devInfo.devId]
  * @returns {Promise<string>} 桶名称
+ * @docs https://developer.tuya.com/cn/docs/app-development/lasersweeper?id=Ka6o1iaxtu71o#title-3-%E6%9B%B4%E6%96%B0%E4%BA%91%E5%AD%98%E5%82%A8%E9%85%8D%E7%BD%AE
  */
 export function updateCloudConfig(devId = TYSdk.devInfo.devId): Promise<string> {
   return new Promise((resolve, reject) => {
@@ -20,6 +21,7 @@ export function updateCloudConfig(devId = TYSdk.devInfo.devId): Promise<string> 
  * @param {string} bucket 桶名称
  * @param {string} filePath 文件相对路径
  * @returns {Promise<string>} 文件的Url
+ * @docs https://developer.tuya.com/cn/docs/app-development/lasersweeper?id=Ka6o1iaxtu71o#title-6-%E8%8E%B7%E5%8F%96%E5%AE%8C%E6%95%B4%E6%96%87%E4%BB%B6%E5%9C%B0%E5%9D%80
  */
 export function getCloudFileUrl(bucket: string, filePath: string): Promise<string> {
   return new Promise((resolve, reject) => {
