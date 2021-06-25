11290fbc76e5464a5b727d98abcf587d3199b886
Joey <Joeynaive@foxmail.com>
2021-06-25 18:37:34 +0800

    feat(ipc): add ty-ipc-ptz component
    * feat(ipc): ty-ipc-ptz
    
    * feat:tyIpcPtz
    
    * fix: eslint fix
    
    * fix: modify style
    
    * feat: new component
    
    * feat(ipc): new component
    
    * feat(ipc): new component
    
    * feat(ipc): new component
    
    * feat(ipc): new component
    
    * chore: add ; to example/src/scenes/Ipc/index.js
    
    * chore: add ; to example/src/scenes/Ipc/index.js
    
    * chore: add ; to example/src/scenes/Ipc/index.js
    
    Co-authored-by: 洛竹 <youngjuning@aliyun.com>

---
+++ packages/tuya-panel-ipc-sdk/src/ty-ipc-ptz/interface.ts
@@ -0,0 +1,30 @@
+import { ViewStyle, ImageStyle } from 'react-native';
+
+export interface TYIpcPtzProps {
+  // 是否禁用
+  disabled: boolean;
+  // ptz宽度
+  pieWidth: number;
+  // ptz高度
+  pieHeight: number;
+  // 主题色
+  themeType: 'light' | 'dark';
+  // 容器样式
+  containerStyle: ViewStyle;
+  // 方向点样式
+  dotStyle: ImageStyle;
+  // 点击函数
+  pressIn?: (index: number) => void;
+  // 离开函数
+  pressOut?: (index) => void;
+  // 激活键颜色
+  activeColor: string;
+  // 是否有上方向点
+  hasPtzUp: boolean;
+  // 是否有下方向点
+  hasPtzDown: boolean;
+  // 是否有左方向点
+  hasPtzLeft: boolean;
+  // 是否有右方向点
+  hasPtzRight: boolean;
+}
