c1edc96db21af42ab7ccc481ff4d939e7f04a2db
Oliverzzq <522872757@qq.com>
2021-06-29 15:50:03 +0800

    f e a t ( g a t e w a y ) :   n e w   g a t e w a y   p o n e n t s   ( # 4 2 )

---
+++ example/src/scenes/Gateway/AddDeviceTip/i18n/strings.ts
@@ -0,0 +1,37 @@
+export default {
+  zh: {
+    addDevice: '添加子设备',
+    normalUsage: '正常使用',
+    changeTextUsage: '修改标题和描述',
+    changeBtnStyleUsage: '修改按钮的样式',
+    changeMaskStyleUsage: '修改遮罩样式',
+    changeContentPaddingHorizontal: '修改内容水平间距',
+
+    tipName1: '传感器',
+    tipContent1: '接通电源,长按复位键(开关)5秒',
+    tipName2: '插座',
+    tipContent2: '接通电源,长按复位键(开关)5秒',
+    tipName3: '光源',
+    tipContent3: '接通电源,开-关-开-关-开',
+    distributionNetworkPrompt: '配网提示',
+    distributionNetworkDesc: '配网描述',
+  },
+
+  en: {
+    addDevice: 'Add device',
+    normalUsage: 'Normal use',
+    changeTextUsage: 'Change title and desc',
+    changeBtnStyleUsage: 'Change style of button ',
+    changeMaskStyleUsage: 'Change mask style',
+    changeContentPaddingHorizontal: 'Change content padding horizontal',
+
+    tipName1: 'Sensor',
+    tipContent1: 'Power on, then hold the RESET button for 5 sec',
+    tipName2: 'Socket',
+    tipContent2: 'Power on, then hold the RESET button for 5 sec',
+    tipName3: 'Light Source',
+    tipContent3: 'Power On, then Turn OFF-ON-OFF-ON',
+    distributionNetworkPrompt: 'Distribution network prompt',
+    distributionNetworkDesc: 'Distribution network description',
+  },
+};
---
+++ example/src/scenes/Gateway/SetPassword/i18n/strings.ts
@@ -0,0 +1,17 @@
+export default {
+  zh: {
+    setPassword: '设置密码',
+    setPasswordLength: '设置密码长度',
+    setCircleActivedColor: '设置圆圈激活状态下颜色',
+    setOnConfirm: '设置onConfirm事件回调',
+    setPasswordSuccess: '设置密码成功',
+  },
+
+  en: {
+    setPassword: 'Set password',
+    setPasswordLength: 'Set password length',
+    setCircleActivedColor: 'Set actived state circle color',
+    setOnConfirm: 'Set onConfirm event callback',
+    setPasswordSuccess: 'Set password success',
+  },
+};
