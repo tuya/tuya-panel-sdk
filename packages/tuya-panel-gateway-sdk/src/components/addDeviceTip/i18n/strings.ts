2bb4438eb4502216ddb1a995f6f5ac74fcb3a68e
Oliverzzq <522872757@qq.com>
2021-06-29 15:50:03 +0800

    f e a t ( g a t e w a y ) :   n e w   g a t e w a y   p o n e n t s   ( # 4 2 )

---
+++ packages/tuya-panel-gateway-sdk/src/components/addDeviceTip/i18n/strings.ts
@@ -0,0 +1,16 @@
+export default {
+  en: {
+    addTitle:
+      'Please make sure the child devices are already in distribution network model (lights flashing)',
+    addDesc:
+      'If the light is not flashing equipment, please reset the device, the following are some common subset of reset:',
+    toAdd: 'Light is in a flash',
+    toMoreSub: 'See more device reset way >',
+  },
+  zh: {
+    addTitle: '请确认子设备已处于配网模式(指示灯闪烁）',
+    addDesc: '若设备指示灯未闪烁，请先重置设备，以下是一些常见子设备的重置方式：',
+    toAdd: '指示灯在快闪',
+    toMoreSub: '查看更多设备重置方式 >',
+  },
+};
