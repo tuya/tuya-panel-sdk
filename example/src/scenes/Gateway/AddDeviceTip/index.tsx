d7f3c911ed18eae798505e400cbd63522aa05fd6
Oliverzzq <522872757@qq.com>
2021-06-29 15:50:03 +0800

    feat(gateway): new gateway components (#42)

---
+++ example/src/scenes/Gateway/AddDeviceTip/index.tsx
@@ -0,0 +1,133 @@
+import React, { FC } from 'react';
+import { StyleSheet, View, ScrollView } from 'react-native';
+import { Button, TYText } from 'tuya-panel-kit';
+import { AddDeviceTipModal } from '@tuya/tuya-panel-gateway-sdk';
+import Strings from './i18n';
+
+const AddDeviceTipScene: FC = () => {
+  const dataSource = [
+    {
+      name: Strings.getLang('tipName1'),
+      icon: 'https://images.tuyacn.com/smart/program_category_icon/zig_afcg.png',
+      content: Strings.getLang('tipContent1'),
+    },
+    {
+      name: Strings.getLang('tipName2'),
+      icon: 'https://images.tuyacn.com/smart/product_icon2/cz_1.png',
+      content: Strings.getLang('tipContent2'),
+    },
+    {
+      name: Strings.getLang('tipName3'),
+      icon: 'https://images.tuyacn.com/smart/product_icon2/dj_1.png',
+      content: Strings.getLang('tipContent3'),
+    },
+  ];
+  const showModalNormal = () => {
+    AddDeviceTipModal.show({
+      dataSource,
+    });
+  };
+  const showModalChangeText = () => {
+    AddDeviceTipModal.show({
+      dataSource,
+      title: Strings.getLang('distributionNetworkPrompt'),
+      desc: Strings.getLang('distributionNetworkDesc'),
+    });
+  };
+  const showModalChangeContentPaddingHorizontal = () => {
+    AddDeviceTipModal.show({
+      dataSource,
+      contentPaddingHorizontal: 40,
+    });
+  };
+  const showModalChangeBtnStyle = () => {
+    AddDeviceTipModal.show({
+      dataSource,
+      addButtonStyle: {
+        backgroundColor: 'lightblue',
+      },
+      addButtonTextStyle: {
+        color: '#333',
+      },
+      moreButtonTextStyle: {
+        color: 'red',
+      },
+    });
+  };
+  const showModalChangeMaskStyle = () => {
+    AddDeviceTipModal.show({
+      dataSource,
+      maskStyle: { backgroundColor: 'pink' },
+    });
+  };
+  const exampleConfigList = [
+    {
+      desc: Strings.getLang('normalUsage'),
+      onPress: showModalNormal,
+    },
+    {
+      desc: Strings.getLang('changeTextUsage'),
+      onPress: showModalChangeText,
+    },
+    {
+      desc: Strings.getLang('changeBtnStyleUsage'),
+      onPress: showModalChangeBtnStyle,
+    },
+    {
+      desc: Strings.getLang('changeContentPaddingHorizontal'),
+      onPress: showModalChangeContentPaddingHorizontal,
+    },
+    {
+      desc: Strings.getLang('changeMaskStyleUsage'),
+      onPress: showModalChangeMaskStyle,
+    },
+  ];
+  return (
+    <View style={styles.containerStyle}>
+      <ScrollView style={styles.scrollView}>
+        {exampleConfigList.map(({ desc, onPress }) => (
+          <View style={styles.row} key={desc}>
+            <TYText style={styles.text} text={desc} />
+            <Button
+              text={Strings.getLang('addDevice')}
+              onPress={onPress}
+              textStyle={{ fontSize: 14 }}
+              style={styles.addBtn}
+            />
+          </View>
+        ))}
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  addBtn: {
+    backgroundColor: '#E5E5E5',
+    borderRadius: 10,
+    margin: 20,
+    padding: 20,
+  },
+  containerStyle: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'flex-start',
+  },
+  row: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 10,
+    width: '100%',
+  },
+  scrollView: {
+    flex: 1,
+    width: '100%',
+  },
+  text: {
+    alignSelf: 'flex-start',
+    // textAlign: 'left',
+    marginLeft: 10,
+  },
+});
+
+export default AddDeviceTipScene;
---
+++ packages/tuya-panel-gateway-sdk/src/components/tempHumWithBlur/res/index.ts
@@ -0,0 +1,7 @@
+const Res = {
+  splitLine: require('./splitLine.png'),
+  humidity: require('./humidity.png'),
+  temperature: require('./temperature.png'),
+};
+
+export default Res;
