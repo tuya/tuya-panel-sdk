fa9a217dea50d3f2349bd4716dcbc538c7da50d4
zhangyaxin <qingli.zhang@tuya.com>
2021-07-12 20:21:53 +0800

    fix: fix some thing

--- example/src/scenes/Electrician/Countdown/index.js
+++ example/src/scenes/Electrician/Countdown/index.js
@@ -6,7 +6,7 @@ export default class Countdown extends PureComponent {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', backgroundColor: 'grey' }}>
-        <CountdownView value={55} counting formatString="开关在{0}:{1}:{2}后关闭" timeUnit="hour" />
+        <CountdownView value={54} counting formatString="开关在{0}:{1}:{2}后关闭" timeUnit="hour" />
       </View>
     );
   }
