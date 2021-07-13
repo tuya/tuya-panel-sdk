193acd87c7dbed4d72c31de85073934cb8d9e349
fuqiu <fuqiu.li@tuya.com>
2021-07-13 16:08:23 +0800

    fix: i18n key update

--- packages/tuya-panel-lock-sdk/src/index.js
+++ packages/tuya-panel-lock-sdk/src/index.js
@@ -3,4 +3,4 @@ export { default as LinkSelect } from './link-select';
 export { default as GetPassword } from './get-password';
 export { default as DefendTime } from './defend-time';
 export { default as Finger } from './finger';
-export { default as DatePickerRange } from './date-picker-view';
+export { default as DatePickerRange } from './date-picker-range';
