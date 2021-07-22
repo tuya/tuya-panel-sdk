331f4938018f10a6aea37c00cf76ae231047c2f9
何良语 <gala.he@tuya.com>
2021-07-22 15:13:46 +0800

    feat: add initStatus, isMuting, isRecording, isTalkBacking, isHDOn methods

--- packages/tuya-panel-ipc-sdk/src/ty-ipc-player/interface.ts
+++ packages/tuya-panel-ipc-sdk/src/ty-ipc-player/interface.ts
@@ -63,6 +63,13 @@ interface TwoMicStyle {
   textTip: any;
 }
 
+interface initStatus {
+  isMuting: boolean;
+  isRecording: boolean;
+  isTalkBacking: boolean;
+  isHDOn: boolean;
+}
+
 const _defaultPlayerLoad = {
   playerLoadContainer: {},
   fullBackIconContainer: {},
@@ -129,6 +136,7 @@ export interface TYIpcPlayerProps {
   // 如进入前台, 从Rn页面返回才会主动触发, 横竖屏转换, 必传
   onChangeActiveZoomStatus: (zoomStatus: number) => any;
   onChangeRecording: (isRecording: boolean) => any;
+  initStatus: (initStatusObj: initStatus) => any;
   zoomStatus: number;
   isFullScreen: boolean;
   fullPlayerWidth: number;
