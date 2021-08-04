551c2001347cb2bda357871a7ce75ecf31a1b90a
qinglizhang <87292374+qinglizhang@users.noreply.github.com>
2021-08-04 09:53:56 +0800

    feat: add panel outdoor sdk(#58)

---
+++ packages/tuya-panel-outdoor-sdk/jest.config.js
@@ -0,0 +1,14 @@
+// Jest configuration for api
+const base = require('../../jest.config.base.js');
+const pkg = require('./package');
+module.exports = {
+  ...base,
+  name: pkg.name,
+  displayName: pkg.name,
+  rootDir: '../..',
+  testMatch: [
+    '<rootDir>/packages/tuya-panel-outdoor-sdk/src/components/**/__tests__/?(*.)+(test).[jt]s?(x)',
+  ],
+  coverageDirectory: '<rootDir>/packages/tuya-panel-outdoor-sdk/coverage/',
+  collectCoverageFrom: ['<rootDir>/packages/tuya-panel-outdoor-sdk/src/components/**/*.[jt]s?(x)'],
+};
---
+++ packages/tuya-panel-outdoor-sdk/src/components/shadow-circle-progress/res/index.tsx
@@ -0,0 +1,8 @@
+export default {
+  // 小阴影
+  progressY:
+    'https://images.tuyacn.com/rms-static/1bab0bb0-d98e-11eb-bb1c-dd1a7461f245-1625048993771.png?tyName=progress-yy%403x.png',
+  // 大阴影
+  progressYY:
+    'https://images.tuyacn.com/rms-static/1bafedb0-d98e-11eb-815d-e39234ce96ff-1625048993803.png?tyName=progress-y%403x.png',
+};
