082941a262c02b110c129717bacdae97b4f22126
杨新林 <2236793638@qq.com>
2021-08-12 16:13:54 +0800

    feat: add progess unlockButton barpercent scrollRuler component (#67)
    * feat: add progess unlockButton barpercent scrollRuler component
    
    * feat: add health panel sdk readme
    
    * test: fix the barPercent test case
    
    * fix(health):fix cr problem, convert SFC to FC
    
    * fix: fix cr problem
    
    Co-authored-by: xl.yang <xl.yang@tuya.com>

--- example/src/scenes/Gateway/index.js
+++ example/src/scenes/Gateway/index.js
@@ -4,13 +4,13 @@ import { produceRouterDatas } from '../../utils';
 import { subRouters } from '../../config/routers';
 import AddDeviceTipScene from './AddDeviceTip';
 import SetPasswordScene from './SetPassword';
-import TopBarWithArcScene from './TopBarWithArc';
+// import TopBarWithArcScene from './TopBarWithArc';
 import TempHumWithBlurcScene from './TempHumWithBlur';
 
 export default class GatewayScene extends Component {
   static AddDeviceTip = AddDeviceTipScene;
   static SetPassword = SetPasswordScene;
-  static TopBarWithArc = TopBarWithArcScene;
+  // static TopBarWithArc = TopBarWithArcScene;
   static TempHumWithBlur = TempHumWithBlurcScene;
 
   get data() {
---
+++ packages/tuya-panel-health-sdk/src/components/barPercent/index.tsx
@@ -0,0 +1,59 @@
+import React, { useMemo, FC } from 'react';
+import { ViewStyle, StyleProp } from 'react-native';
+import { BarList } from './BarList';
+
+export const sum = (arr: number[]) => {
+  if (arr.length === 0) return arr;
+  return [...arr].reduce((acc, val) => acc + val, 0);
+};
+
+export const WAKE_COLOR = '#FFCF3A';
+export const LIGHT_COLOR = '#E5C7F2';
+export const DEEP_COLOR = '#BF73DE';
+export const REM_COLOR = '#F8F1FB';
+
+export const DEFAULT_COLORS = {
+  Wake: WAKE_COLOR,
+  Light: LIGHT_COLOR,
+  Deep: DEEP_COLOR,
+  REM: REM_COLOR,
+};
+
+// 0-清醒；1-浅睡；2-深睡；3-REM（可能没有）
+// eslint-disable-next-line no-shadow
+export enum SleepStatus {
+  'Wake' = 'Wake',
+  'Deep' = 'Deep',
+  'Light' = 'Light',
+  'REM' = 'REM',
+}
+
+export interface Data {
+  value: number;
+  label: string;
+  key?: string;
+  color?: string;
+}
+export interface BarPercentProps {
+  data: Data[];
+  style?: StyleProp<ViewStyle>;
+}
+
+const BarPercent: FC<BarPercentProps> = (props: BarPercentProps) => {
+  const { data, style } = props;
+  const total = useMemo(() => sum(data.map(d => +d.value)) as number, [JSON.stringify(data)]);
+  const { length } = data;
+  const renderItem = (item, index) => {
+    const flexPercent = (item.value / total) * length;
+    return (
+      <BarList.Item
+        key={item?.key || index}
+        flexPercent={flexPercent}
+        backgroundColor={item.color || DEFAULT_COLORS[item.key]}
+      />
+    );
+  };
+  return <BarList dataSource={data} style={style} renderItem={renderItem} />;
+};
+
+export default BarPercent;
