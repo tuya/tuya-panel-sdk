20511f6346582e53f341e62d4a65d29bd06b8741
Joey <Joeynaive@foxmail.com>
2021-08-24 19:56:53 +0800

    f e a t ( i p c ) :   a d d   g p s   s i g n a l   a n d   m e s s a g e   p l a y e r   p o n e n t   ( # 7 6 )
      *   f e a t ( i p c ) :   a d d   g p s   s i g n a l   a n d   m e s s a g e   p l a y e r   p o n e n t  
       
      *   c h o r e :   r e v i e w   f i x  
       
      C o - a u t h o r e d - b y :   洛 竹   < y o u n g j u n i n g @ a l i y u n   >

---
+++ packages/tuya-panel-ipc-sdk/src/ty-ipc-gps-signal/__tests__/tyIpcGpsSignal.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+describe('TYIpcGpsSignal components', () => {
+  it('basic render', () => {
+    const wrapper = shallow(
+      <TYIpcGpsSignal containerStyle={{ marginLeft: 50 }} gpsSignal={0} lteSignal={50} />
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+  it('gps render', () => {
+    const wrapper = shallow(<TYIpcGpsSignal containerStyle={{ marginLeft: 50 }} gpsSignal={0} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+  it('lte render', () => {
+    const wrapper = shallow(<TYIpcGpsSignal containerStyle={{ marginLeft: 50 }} lteSignal={50} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
