143925361cb36d6cc77108d45ee0c60cb3da2b1d
洛竹 <youngjuning@aliyun.com>
2021-09-01 16:53:01 +0800

    c h o r e ( e x a m p l e ) :   m e n t   I p c

--- example/metro.config.js
+++ example/metro.config.js
@@ -5,9 +5,10 @@ const modules = ['tuya-panel-kit', 'react', 'react-native', 'react-native-svg'];
 
 const root = path.resolve(__dirname, '..');
 
-const blacklistRE = blacklist(
-  modules.map(m => new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`))
-);
+const blacklistRE = blacklist([
+  ...modules.map(m => new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)),
+  new RegExp(`^${escape(path.join(root, 'packages/tuya-panel-lamp-sdk/node_modules'))}\\/.*$`),
+]);
 
 const extraNodeModules = modules.reduce((acc, name) => {
   acc[name] = path.join(__dirname, 'node_modules', name);
