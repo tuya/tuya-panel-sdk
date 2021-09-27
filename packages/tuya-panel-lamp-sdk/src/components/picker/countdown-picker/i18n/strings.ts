d8707f01fba14e3d29bba0e9acead2bb62c7b399
Andy <andyyxw@163.com>
2021-09-27 10:43:56 +0800

    feat(lamp): Add CountdownPicker component (#105)

---
+++ packages/tuya-panel-lamp-sdk/src/components/picker/countdown-picker/i18n/strings.ts
@@ -0,0 +1,12 @@
+export default {
+  en: {
+    CountdownPicker_hour: 'H',
+    CountdownPicker_minute: 'M',
+    CountdownPicker_second: 'S',
+  },
+  zh: {
+    CountdownPicker_hour: '小时',
+    CountdownPicker_minute: '分钟',
+    CountdownPicker_second: '秒',
+  },
+};
