a9b47e32bc5a733ddb45ca94b8061f4fec79b065
洛竹 <youngjuning@163.com>
2021-10-25 16:51:44 +0800

    feat: add outdoor new component:DateText(#111)

--- packages/tuya-panel-outdoor-sdk/src/index.js
+++ packages/tuya-panel-outdoor-sdk/src/index.js
@@ -1,3 +1,5 @@
 export { default as LocationText } from './components/location-text';
 export { default as SettingSlider } from './components/setting-slider';
 export { default as ShadowCircleProgress } from './components/shadow-circle-progress';
+export { default as DateText } from './components/date-text';
+export { parseSecondToDate } from './components/date-text/util';
