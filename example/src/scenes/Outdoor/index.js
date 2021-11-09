e7a74bdad870075a97865bfa424b9d3d7712e2e2
yufenfen <fangao.zhang@tuya.com>
2021-11-09 15:58:02 +0800

    feat(tuya-panel-outdoor-sdk): add calendarlist component

--- example/src/scenes/Outdoor/index.js
+++ example/src/scenes/Outdoor/index.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { TYFlatList } from 'tuya-panel-kit';
 import { subRouters } from '../../config/routers';
 import { produceRouterDatas } from '../../utils';
 import LocationText from './location-text';
 import DateText from './date-text';
+import CalendarList from './calendar-list';
 import SettingSlider from './setting-slider';
 import CircleProgress from './shadow-circle-progress';
 
-export default class OutdoorScene extends Component {
+export default class OutdoorScene extends React.Component {
   get data() {
     return produceRouterDatas(subRouters.filter(r => /^Outdoor\.((?!\.)\w)+$/.test(r.id)));
   }
@@ -16,6 +17,7 @@ export default class OutdoorScene extends Component {
   static LocationText = LocationText;
   static SettingSlider = SettingSlider;
   static shadowCircleProgress = CircleProgress;
+  static CalendarList = CalendarList;
 
   render() {
     return <TYFlatList data={this.data} />;
