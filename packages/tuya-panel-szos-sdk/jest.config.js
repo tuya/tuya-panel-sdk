1c322c327aea01e10cc070ccec10720638caf018
丁宁 <jingyin.ding@tuya.com>
2021-11-11 17:05:59 +0800

    feat(tuya-panel-szos-sdk): jest config only check component

--- packages/tuya-panel-szos-sdk/jest.config.js
+++ packages/tuya-panel-szos-sdk/jest.config.js
@@ -2,11 +2,13 @@
 const base = require('../../jest.config.base.js');
 const pkg = require('./package');
 module.exports = {
-    ...base,
-    name: pkg.name,
-    displayName: pkg.name,
-    rootDir: '../..',
-    testMatch: ['<rootDir>/packages/tuya-panel-szos-sdk/src/**/__tests__/?(*.)+(test).[jt]s?(x)'],
-    coverageDirectory: '<rootDir>/packages/tuya-panel-szos-sdk/coverage/',
-    collectCoverageFrom: ['<rootDir>/packages/tuya-panel-szos-sdk/src/**/*.[jt]s?(x)'],
+  ...base,
+  name: pkg.name,
+  displayName: pkg.name,
+  rootDir: '../..',
+  testMatch: [
+    '<rootDir>/packages/tuya-panel-szos-sdk/src/components/**/__tests__/?(*.)+(test).[jt]s?(x)',
+  ],
+  coverageDirectory: '<rootDir>/packages/tuya-panel-szos-sdk/coverage/',
+  collectCoverageFrom: ['<rootDir>/packages/tuya-panel-szos-sdk/src/components/**/*.[jt]s?(x)'],
 };
