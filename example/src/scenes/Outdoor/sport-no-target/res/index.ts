1448cd05da0567afd6a6cb54c7a4fe7f2a78630a
huxuepeng999 <huxuepeng999@users.noreply.github.com>
2021-11-18 16:33:51 +0800

    feat: sport page, target and no target component (#123)

---
+++ example/src/scenes/Outdoor/sport-no-target/res/index.ts
@@ -0,0 +1,10 @@
+/* eslint-disable camelcase */
+const sport_border_circle1 = require('./image/sport_border_circle1.png');
+const step_spot1 = require('./image/step_spot1.png');
+const sport_target_content = require('./image/sport_target_content.png');
+
+export default {
+  step_spot1,
+  sport_border_circle1,
+  sport_target_content,
+};
