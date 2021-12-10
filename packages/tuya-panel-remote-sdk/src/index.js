fd8de3018009bd02222ec1ffcebcff2d9f5ab383
洛竹 <luozhu.yang@tuya.com>
2021-12-10 11:25:12 +0800

    feat(tuya-panel-remote-sdk): add some component (#126)

--- packages/tuya-panel-remote-sdk/src/index.js
+++ packages/tuya-panel-remote-sdk/src/index.js
@@ -5,5 +5,14 @@ import DoubleKey from './components/double-key';
 import CurtainsAnimate from './components/curtains-animate';
 import PusherAnimate from './components/pusher-animate';
 import RollerAnimate from './components/roller-animate';
+import LightAnimate from './components/light-animate';
 
-export { CircleHandle, PressKey, DoubleKey, CurtainsAnimate, PusherAnimate, RollerAnimate };
+export {
+  CircleHandle,
+  PressKey,
+  DoubleKey,
+  CurtainsAnimate,
+  PusherAnimate,
+  RollerAnimate,
+  LightAnimate,
+};
