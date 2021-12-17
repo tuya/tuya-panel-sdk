9ec09b0659a296b52f14fc263287251557230ca6
Sans1216 <48463004+sans1216@users.noreply.github.com>
2021-12-17 15:24:37 +0800

    feat: add colorRect and musicDrawer components (#139)

--- example/src/i18n/strings.js
+++ example/src/i18n/strings.js
@@ -11,6 +11,18 @@ module.exports = {
     TY_dp_switch_1_off: '关',
     TYLamp_am: '上午',
     TYLamp_pm: '下午',
+
+    TYLamp_mode: '模式',
+    TYLamp_unSelected: '未选中状态',
+    TYLamp_selected: '选中状态',
+    TYLamp_customizeStyle: '自定义样式',
+    TYLamp_customizeContent: '自定义内容',
+    TYLamp_drawerContent: '下拉内容',
+    TYLamp_subTitle: '子标题',
+    TYLamp_customizeDescription: '没有左边按钮，有子标题，按钮固定',
+
+    TYLamp_average: '平均分配',
+    TYLamp_percent: '按百分比分配',
   },
 
   en: {
@@ -21,5 +33,17 @@ module.exports = {
     TY_Sensor: '传感器(Sensor)',
     TYLamp_am: 'AM',
     TYLamp_pm: 'PM',
+
+    TYLamp_mode: 'mode',
+    TYLamp_unSelected: 'Unselected State',
+    TYLamp_selected: 'Selected State',
+    TYLamp_customizeStyle: 'Customize',
+    TYLamp_customizeContent: 'Customize Content',
+    TYLamp_drawerContent: 'Drawer Content',
+    TYLamp_subTitle: 'Subtitle',
+    TYLamp_customizeDescription: 'Has no left icon,but has subtitle,only one button image',
+
+    TYLamp_average: 'Equally Calculate',
+    TYLamp_percent: 'Calculate by percentage',
   },
 };
