f70cfb067ecd0ab54af2df110b28e30e7a85ddfb
Sans1216 <48463004+sans1216@users.noreply.github.com>
2021-12-17 16:17:34 +0800

    fix: update musicDrawer test file (#141)

--- packages/tuya-panel-lamp-sdk/src/components/music/music-drawer/__tests__/music-drawer.test.js
+++ packages/tuya-panel-lamp-sdk/src/components/music/music-drawer/__tests__/music-drawer.test.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { View } from 'react';
 import { shallow } from 'enzyme';
 import MusicDrawer from '../index';
 import res from '../../../../res/index';
@@ -21,10 +21,24 @@ const DrawerData2 = videoListData.map((item, index) => ({
   },
   leftIconSource: res.mode1,
 }));
+const DrawerData3 = videoListData.map((item, index) => ({
+  id: item,
+  title: `title${index}`,
+  subTitle: `subTitle${index}`,
+  leftIconSource: res.mode1,
+}));
 describe('MusicBar components', () => {
   it('basic render no active', () => {
     const wrapper = shallow(
-      <MusicDrawer value={DrawerData1} activeIndex={-1} activeHeight={306} height={102} />
+      <MusicDrawer
+        value={DrawerData1}
+        activeIndex={-1}
+        activeHeight={306}
+        height={102}
+        renderActiveContent={() => {
+          return <View styles={{ containerStyle: { height: 300 } }} />;
+        }}
+      />
     );
 
     expect(wrapper).toMatchSnapshot();
@@ -44,6 +58,31 @@ describe('MusicBar components', () => {
         activeIndex={0}
         activeHeight={200}
         height={80}
+        renderActiveContent={() => {
+          return <View styles={{ containerStyle: { height: 200 } }} />;
+        }}
+      />
+    );
+
+    expect(wrapper).toMatchSnapshot();
+  });
+  it('basic render with no right icon', () => {
+    const wrapper = shallow(
+      <MusicDrawer
+        styles={{
+          contentStyle: { backgroundColor: 'pink' },
+          titleStyle: { fontSize: 16 },
+          subTitleStyle: { fontSize: 12 },
+          containerStyle: { width: 420 },
+          leftIconBoxStyle: { backgroundColor: 'red' },
+        }}
+        value={DrawerData3}
+        activeIndex={1}
+        activeHeight={200}
+        height={80}
+        renderActiveContent={() => {
+          return <View styles={{ containerStyle: { height: 200 } }} />;
+        }}
       />
     );
 
