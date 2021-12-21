1f6c31adaec5f5e219b47de413f0aeb230e80da6
洛竹 <luozhu.yang@tuya.com>
2021-12-21 10:15:07 +0800

    chore: add jest.setup.js

---
+++ jest.setup.js
@@ -0,0 +1,14 @@
+import { NativeModules } from 'react-native';
+import 'react-native-mock-render/mock';
+import 'react-test-renderer';
+import 'react-native/Libraries/Animated/src/bezier'; // for https://github.com/facebook/jest/issues/4710
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+NativeModules.RNGestureHandlerModule = {};
+NativeModules.TYRCTPublicModule = {};
+NativeModules.TYRCTDeviceModule = {};
+NativeModules.TYRCTPublicManager = {};
+NativeModules.TYRCTPanelManager = {};
+
+Enzyme.configure({ adapter: new Adapter() });
