a6ed10a981e679c4ec9691b1c9d35aaca2505ab9
小林爱学习 <52225311+UnicornLinYI@users.noreply.github.com>
2021-12-28 14:55:38 +0800

    feat: add Bubble and DargSort (#156)

---
+++ packages/tuya-panel-lock-sdk/src/bubble-tip/icons/index.tsx
@@ -0,0 +1,4 @@
+/* eslint-disable import/no-unresolved */
+export const safe = require('./icon_safe.png');
+export const info = require('./icon_info.png');
+export const danger = require('./icon_danger.png');
---
+++ packages/tuya-panel-lock-sdk/src/drag-sort/index.tsx
@@ -0,0 +1,3 @@
+import DragSortReview from './DragSort';
+
+export default DragSortReview;
