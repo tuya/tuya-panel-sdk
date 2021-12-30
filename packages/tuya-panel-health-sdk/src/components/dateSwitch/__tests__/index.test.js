11ebdbe4326f6f561a8a2042ca0275c8562cf63b
折木 <xl.yang@tuya.com>
2021-12-30 23:14:04 +0800

    feat: Add unit test

--- packages/tuya-panel-health-sdk/src/components/dateSwitch/__tests__/index.test.js
+++ packages/tuya-panel-health-sdk/src/components/dateSwitch/__tests__/index.test.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { View } from 'react-native';
 import { shallow } from 'enzyme';
 import { DaySwitch, WeekSwitch, MonthSwitch } from '../index';
+import Header from '../Header';
 
 describe('DaySwitch components', () => {
   it('basic DaySwitch render', () => {
     const wrapper = shallow(<DaySwitch value={Date.now()} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('basic Header render', () => {
+    const wrapper = shallow(<Header value="2022" onPrev={() => {}} onNext={() => {}} />);
     expect(wrapper).toMatchSnapshot();
   });
 
@@ -15,12 +21,26 @@ describe('DaySwitch components', () => {
   });
 
   it('basic WeekSwitch render', () => {
-    const wrapper = shallow(<WeekSwitch />);
+    const wrapper = shallow(
+      <WeekSwitch
+        value={[Date.now(), Date.now()]}
+        onChange={dateString => {
+          console.log(dateString);
+        }}
+      />
+    );
     expect(wrapper).toMatchSnapshot();
   });
 
   it('basic MonthSwitch render', () => {
-    const wrapper = shallow(<MonthSwitch />);
+    const wrapper = shallow(
+      <MonthSwitch
+        value={[Date.now(), Date.now()]}
+        onChange={(dateString, date) => {
+          console.log(dateString);
+        }}
+      />
+    );
     expect(wrapper).toMatchSnapshot();
   });
 });
