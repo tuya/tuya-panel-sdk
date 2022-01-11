66698381f5a9804763c21e5d38e73559a7f4dfe9
Paruki.Gin <31382985+paruki-gin@users.noreply.github.com>
2022-01-11 14:38:58 +0800

    feat: add lamp general api (#163)

--- packages/tuya-panel-api/src/lamp/index.ts
+++ packages/tuya-panel-api/src/lamp/index.ts
@@ -1,7 +1,9 @@
 import timingApi from './timing';
 import sceneApi from './scene';
+import generalApi from './general';
 
 export default {
   timingApi,
   sceneApi,
+  generalApi,
 };
--- packages/tuya-panel-api/src/lamp/utils.ts
+++ packages/tuya-panel-api/src/lamp/utils.ts
@@ -1,22 +1,27 @@
-export const parseJSON = (str: string) => {
-  let rst;
-  if (str && {}.toString.call(str) === '[object String]') {
-    // 当JSON字符串解析
-    try {
-      rst = JSON.parse(str);
-    } catch (e) {
-      // 出错，用eval继续解析JSON字符串
-      try {
-        // eslint-disable-next-line
-        rst = eval(`(${str})`);
-      } catch (e2) {
-        // 当成普通字符串
-        rst = str;
-      }
-    }
-  } else {
-    rst = typeof str === 'undefined' ? {} : str;
-  }
+/* eslint-disable no-console */
+import { TYSdk } from 'tuya-panel-kit';
 
-  return rst;
-};
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export function api(a: string, postData: any, v = '1.0'): Promise<any> {
+  const sucStyle = 'background: green; color: #fff;';
+  const errStyle = 'background: red; color: #fff;';
+  return new Promise((resolve, reject) => {
+    TYSdk.native.apiRNRequest(
+      {
+        a,
+        postData,
+        v,
+      },
+      (d: string) => {
+        const data = typeof d === 'string' ? JSON.parse(d) : d;
+        console.log(`API Success: %c${a}%o`, sucStyle, data);
+        resolve(data);
+      },
+      (err: string) => {
+        const e = typeof err === 'string' ? JSON.parse(err) : err;
+        console.log(`API Failed: %c${a}%o`, errStyle, e.message || e.errorMsg || e);
+        reject(err);
+      }
+    );
+  });
+}
