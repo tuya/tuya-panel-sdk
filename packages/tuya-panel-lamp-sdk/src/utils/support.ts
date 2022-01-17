6e6a0d2ad1d635ec339a39f6757cbbcd073f2c2f
jinxiao <jinxiao@tuya.com>
2022-01-17 20:39:03 +0800

    fix: lamp support utils fix

--- packages/tuya-panel-lamp-sdk/src/utils/support.ts
+++ packages/tuya-panel-lamp-sdk/src/utils/support.ts
@@ -1,7 +1,6 @@
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import dpCodes from './dpCodes';
 import _get from 'lodash/get';
 import { TYSdk } from 'tuya-panel-kit';
+import dpCodes from './dpCodes';
 
 const cache: any = {};
 
@@ -16,6 +15,11 @@ const {
   rhythmCode,
 } = dpCodes;
 
+const supportDp = (code: string) => {
+  const { schema } = TYSdk.devInfo;
+  return !!schema[code];
+};
+
 const supportWorkMode = (code: string) => {
   const { schema } = TYSdk.devInfo;
   const workModeRange: string[] = _get(schema[workModeCode], 'range') || [];
@@ -25,7 +29,7 @@ const supportWorkMode = (code: string) => {
 const isSupportByDpAndWorkMode = (
   code: string,
   dpCode: string,
-  workmode: string,
+  workMode: string,
   isForce: boolean
 ) => {
   if (!isForce) {
@@ -33,7 +37,10 @@ const isSupportByDpAndWorkMode = (
       return cache[code];
     }
   }
-  const isSupport = false;
+  // 是否存在相关dp
+  const isDpSupport = supportDp(dpCode);
+  const isInWorkMode = supportWorkMode(workMode);
+  const isSupport = isDpSupport && isInWorkMode;
   cache[code] = isSupport;
   return isSupport;
 };
@@ -44,86 +51,107 @@ const isSupportByDp = (code: string, dpCode: string, isForce: boolean) => {
       return cache[code];
     }
   }
-  const isSupport = false;
+  // 是否存在相关dp
+  const isDpSupport = supportDp(dpCode);
+  cache[code] = isDpSupport;
+  return isDpSupport;
+};
+
+// 群组设备
+const isGroupDevice = (): boolean => {
+  return !!TYSdk.devInfo.groupId;
+};
+
+// 支持白光亮度
+const isSupportBright = (isForce = false): boolean => {
+  return isSupportByDpAndWorkMode('isSupportBright', brightCode, 'white', isForce);
+};
+
+// 支持色温
+const isSupportTemp = (isForce = false): boolean => {
+  return isSupportByDpAndWorkMode('isSupportTemp', temperatureCode, 'white', isForce);
+};
+
+// 支持彩光
+const isSupportColour = (isForce = false): boolean => {
+  return isSupportByDpAndWorkMode('isSupportColour', colourCode, 'colour', isForce);
+};
+
+// 支持场景
+const isSupportScene = (isForce = false): boolean => {
+  return isSupportByDpAndWorkMode('isSupportScene', sceneCode, 'scene', isForce);
+};
+
+// 支持音乐
+const isSupportMusic = (isForce = false): boolean => {
+  return isSupportByDpAndWorkMode('isSupportMusic', musicCode, 'music', isForce);
+};
+
+// 支持倒计时
+const isSupportCountdown = (isForce = false): boolean => {
+  return isSupportByDp('isSupportCountdown', countdownCode, isForce);
+};
+
+// 支持生物节律
+const isSupportRhythm = (isForce = false): boolean => {
+  return isSupportByDp('isSupportRhythm', rhythmCode, isForce);
+};
+
+// 支持白光
+const isSupportWhite = (isForce = false): boolean => {
+  const code = 'isSupportWhite';
+  if (!isForce) {
+    if (cache[code]) {
+      return cache[code];
+    }
+  }
+  const isSupport = isSupportBright(true) || isSupportTemp(true);
   cache[code] = isSupport;
-  return isSupport;
+  return cache[code];
 };
 
-const SupportUtils = {
-  // 群组设备
-  isGroupDevice() {
-    return !!TYSdk.devInfo.groupId;
-  },
-  // 支持白光亮度
-  isSupportBright(isForce = false) {
-    return isSupportByDpAndWorkMode('isSupportBright', brightCode, 'white', isForce);
-  },
-  // 支持色温
-  isSupportTemp(isForce = false) {
-    return isSupportByDpAndWorkMode('isSupportTemp', temperatureCode, 'white', isForce);
-  },
-  // 支持彩光
-  isSupportColour(isForce = false) {
-    return isSupportByDpAndWorkMode('isSupportColour', colourCode, 'colour', isForce);
-  },
-  // 支持场景
-  isSupportScene(isForce = false) {
-    return isSupportByDpAndWorkMode('isSupportScene', sceneCode, 'scene', isForce);
-  },
-  // 支持音乐
-  isSupportMusic(isForce = false) {
-    return isSupportByDpAndWorkMode('isSupportMusic', musicCode, 'music', isForce);
-  },
-  // 支持倒计时
-  isSupportCountdown(isForce = false) {
-    return isSupportByDp('isSupportCountdown', countdownCode, isForce);
-  },
-  // 支持生物节律
-  isSupportRhythm(isForce = false) {
-    return isSupportByDp('isSupportRhythm', rhythmCode, isForce);
-  },
-  // 支持白光
-  isSupportWhite(isForce = false) {
-    const code = 'isSupportWhite';
-    if (!isForce) {
-      if (cache[code]) {
-        return cache[code];
-      }
-    }
-
-    const isSupport = false;
-    cache[code] = isSupport;
-    return cache[code];
-  },
-  isSupportWorkMode(code: string) {
-    return supportWorkMode(code);
-  },
-  isSupportDp(dpCode: string, isForce = false) {
-    const code = `isSupport_${dpCode}`;
-    if (!isForce) {
-      if (cache[code]) {
-        return cache[code];
-      }
-    }
-    const isSupport = false;
-    cache[code] = isSupport;
-    return cache[code];
-  },
-  hasCapability(id: number) {
-    // eslint-disable-next-line no-bitwise
-    return (TYSdk.devInfo.capability & (1 << id)) > 0;
-  },
-  // Zigbee设备
-  isZigbeeDevice() {
-    return this.hasCapability(12);
-  },
-  // SigMesh设备
-  isSigMeshDevice() {
-    return this.hasCapability(15);
-  },
-  isWifiDevice() {
-    return this.hasCapability(1);
-  },
+const isSupportWorkMode = (code: string): boolean => {
+  return supportWorkMode(code);
 };
 
-export default SupportUtils;
+const isSupportDp = (dpCode: string, isForce = false): boolean => {
+  return isSupportByDp(`isSupport_${dpCode}`, dpCode, isForce);
+};
+
+const hasCapability = (id: number): boolean => {
+  // eslint-disable-next-line no-bitwise
+  return (TYSdk.devInfo.capability & (1 << id)) > 0;
+};
+
+// Zigbee Device
+const isZigbeeDevice = (): boolean => {
+  return hasCapability(12);
+};
+
+// SigMesh Device
+const isSigMeshDevice = (): boolean => {
+  return hasCapability(15);
+};
+
+// WiFi Device
+const isWifiDevice = (): boolean => {
+  return hasCapability(1);
+};
+
+export default {
+  isGroupDevice,
+  isSupportBright,
+  isSupportTemp,
+  isSupportColour,
+  isSupportScene,
+  isSupportMusic,
+  isSupportCountdown,
+  isSupportRhythm,
+  isSupportWhite,
+  isSupportWorkMode,
+  isSupportDp,
+  hasCapability,
+  isZigbeeDevice,
+  isSigMeshDevice,
+  isWifiDevice,
+};
