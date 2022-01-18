8b9cdc8a620acad0b2607b86434da23eca679326
Oliverzzq <522872757@qq.com>
2022-01-18 10:41:38 +0800

    feat(gateway): Add a new open API of gateway. Add a new util to transform mac. (#168)

--- packages/tuya-panel-api/src/gateway/relation.ts
+++ packages/tuya-panel-api/src/gateway/relation.ts
@@ -1,16 +1,16 @@
 import { TYSdk } from 'tuya-panel-kit';
-import { AddBleSubDev } from './interface';
+import { BleNodeInfo, BeaconNodeInfo } from './interface';
 
 /**
- * @description: 蓝牙单点设备转移到指定网关下
- * @param {*} sourceMeshId meshId或网关id，子设备当前所属，蓝牙单点传网关id，mesh设备传meshId，同时有mesh和蓝牙单点传meshId
- * @param {*} nodes 设备id和虚拟Id列表，例如:[{"devId":"002003545ccf7f34af1e", "uuid":"2334d72777532f8e"},{"devId":"002003545ccf7f34af1e", "uuid":"2334d72777532f8e"}]
- * @param {*} targetMeshId meshId或网关id，子设备目标所属
+ * @description: 绑定 BLE 设备到网关、从网关解绑 BLE 设备。
+ * @param {*} sourceMeshId 子设备当前所属的 meshId 或网关设备 ID。绑定到网关下时，传网关的 sigmeshId。从网关解绑时，传网关的设备 ID。
+ * @param {*} nodes BLE 设备信息列表
+ * @param {*} targetMeshId 绑定目标的 meshId。绑定到网关下时，传网关的设备 ID。从网关解绑时，传 null。
  * @return {*}
  */
 const bleSubDevRelationUpdate = (
   sourceMeshId: string,
-  nodes: AddBleSubDev[],
+  nodes: BleNodeInfo[],
   targetMeshId: string | null
 ): Promise<boolean> => {
   return TYSdk.apiRequest(
@@ -21,16 +21,16 @@ const bleSubDevRelationUpdate = (
 };
 
 /**
- * @description: SIG Mesh子设备的添加和删除
- * @param {*} sourceMeshId meshId或网关id，子设备当前所属，蓝牙单点传网关id，mesh设备传meshId，同时有mesh和蓝牙单点传meshId
- * @param {*} nodeIds 子设备列表,蓝牙单点设备传uuid，mesh设备传nodeId
- * @param {*} targetMeshId meshId或网关id，子设备目标所属
+ * @description: 绑定 SIG MESH 设备到网关、从网关解绑 SIG MESH 设备。
+ * @param {*} sourceMeshId 子设备当前所属的 meshId 或网关设备 ID。绑定到网关下时，传子设备的 meshId。从网关解绑时，传网关的设备 ID。
+ * @param {*} nodeIds SIG MESH 设备 nodeId 列表
+ * @param {*} targetMeshId 绑定目标的 meshId。绑定到网关下时，传网关的设备 ID。从网关解绑时，传 null。
  * @return {*}
  */
 const sigmeshSubDevRelationUpdate = (
   sourceMeshId: string,
   nodeIds: string[],
-  targetMeshId: string
+  targetMeshId: string | null
 ): Promise<boolean> => {
   return TYSdk.apiRequest(
     'tuya.m.device.relation.update',
@@ -39,7 +39,27 @@ const sigmeshSubDevRelationUpdate = (
   );
 };
 
+/**
+ * @description: 绑定 Beacon 设备到网关、从网关解绑 Beacon 设备。
+ * @param {*} sourceMeshId 子设备当前所属的网关设备 ID。绑定到网关下时，传null。从网关解绑时，传网关的设备 ID。
+ * @param {*} nodeIds Beacon 设备信息列表（mac要求小写并且去掉冒号）
+ * @param {*} targetMeshId 绑定目标的 meshId。绑定到网关下时，传网关的设备 ID。从网关解绑时，传 null。
+ * @return {*}
+ */
+const beaconSubDevRelationUpdate = (
+  sourceMeshId: string | null,
+  nodeIds: BeaconNodeInfo[],
+  targetMeshId: string | null
+): Promise<boolean> => {
+  return TYSdk.apiRequest(
+    'tuya.m.device.relation.update.for.beacon',
+    { sourceMeshId, nodeIds, targetMeshId },
+    '2.0'
+  );
+};
+
 export default {
   bleSubDevRelationUpdate,
   sigmeshSubDevRelationUpdate,
+  beaconSubDevRelationUpdate,
 };
