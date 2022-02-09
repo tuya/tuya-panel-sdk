510edabcccd961af0e9d67e2ba3465422ac1f951
zesenzhu <46235334+zesenzhu@users.noreply.github.com>
2022-02-09 18:28:26 +0800

    feat: add TurnPlate、SimpleVerticalSlider components (#164)

---
+++ packages/tuya-panel-szos-sdk/src/components/steering-wheel/README.md
@@ -0,0 +1,29 @@
+## @tuya/tuya-panel-szos-sdk
+
+English | [简体中文](./README-zh_CN.md)
+
+Tuya szos component library
+
+[![](https://img.shields.io/npm/v/@tuya/tuya-panel-szos-sdk/latest.svg)](https://www.npmjs.com/package/@tuya/tuya-panel-szos-sdk)
+
+## Install
+
+```sh
+$ npm install @tuya/tuya-panel-szos-sdk
+// or
+$ yarn add @tuya/tuya-panel-szos-sdk
+```
+
+## Usage
+
+```js
+import { SoundWave } from '@tuya/tuya-panel-szos-sdk';
+```
+
+## Doc
+
+For more documentation on szos components, please refer to [Tuya Design](https://developer.tuya.com/cn/docs/control-panel-sdk/szos-sdk?id=Kaiuyhfamrhom)
+
+## Attention
+目前是以正北方向为基准，后续有时间再扩展任意角度基准
+父元素必须是RN面板最根视图，后续有时间再扩展
---
+++ packages/tuya-panel-szos-sdk/src/components/steering-wheel/__tests__/steering-wheel.test.js
@@ -0,0 +1,193 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from 'react';
+import { View } from 'react-native';
+import { shallow, useStateMock } from 'enzyme';
+import { State } from 'react-native-gesture-handler';
+import SteeringWheel from '../index';
+
+describe('StreeringWheel components', () => {
+  jest.useFakeTimers();
+  it('basic render', () => {
+    const left = 0;
+    const top = 0;
+    const driveRef = { current: null };
+    const wheelLeftRef = { current: null };
+    const wrapper = shallow(
+      <SteeringWheel
+        wheelLeftRef={wheelLeftRef}
+        leftPart={left}
+        topPart={top}
+        driveRef={driveRef}
+      />
+    );
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('content render', () => {
+    const props = {
+      leftPart: 0,
+      topPart: 0,
+      childrenProps: (
+        <View style={{ borderColor: 'red', borderWidth: 1, width: 167, height: 167 }} />
+      ),
+    };
+    const wrapper = shallow(<SteeringWheel {...props} />);
+    const wrapperHandle = wrapper.children().at(0);
+    wrapper.simulate('layout', {
+      nativeEvent: {
+        layout: { x: 200, y: 277, width: 167, height: 167 },
+      },
+    });
+    wrapperHandle.simulate('handlerStateChange', {
+      nativeEvent: {
+        state: State.BEGAN,
+        absoluteX: 250,
+        absoluteY: 298,
+      },
+    });
+    wrapperHandle.simulate('handlerStateChange', {
+      nativeEvent: {
+        state: State.ACTIVE,
+        absoluteX: 263,
+        absoluteY: 291,
+      },
+    });
+    wrapperHandle.simulate('handlerStateChange', {
+      nativeEvent: {
+        state: State.ACTIVE,
+        absoluteX: 246,
+        absoluteY: 356,
+      },
+    });
+    wrapperHandle.simulate('handlerStateChange', {
+      nativeEvent: {
+        state: State.ACTIVE,
+        absoluteX: 888,
+        absoluteY: 888,
+      },
+    });
+    wrapperHandle.simulate('handlerStateChange', {
+      nativeEvent: {
+        state: State.END,
+        absoluteX: 184,
+        absoluteY: 359,
+      },
+    });
+    wrapperHandle.simulate('handlerStateChange', {
+      nativeEvent: {
+        state: State.BEGAN,
+        absoluteX: 888,
+        absoluteY: 888,
+      },
+    });
+    wrapperHandle.simulate('handlerStateChange', {
+      nativeEvent: {
+        state: State.CANCELLED,
+        absoluteX: 184,
+        absoluteY: 359,
+      },
+    });
+    wrapperHandle.simulate('handlerStateChange', {
+      nativeEvent: {
+        state: State.FAILED,
+        absoluteX: 184,
+        absoluteY: 359,
+      },
+    });
+    wrapperHandle.simulate('handlerStateChange', {
+      nativeEvent: {
+        state: State.UNDETERMINED,
+        absoluteX: 184,
+        absoluteY: 359,
+      },
+    });
+    wrapperHandle.simulate('handlerStateChange', {
+      nativeEvent: {
+        state: State.shallow,
+        absoluteX: 184,
+        absoluteY: 359,
+      },
+    });
+    expect(wrapper).toMatchSnapshot();
+
+    wrapper.unmount();
+  });
+
+  it('render with value', () => {
+    jest.spyOn(React, 'useState').mockImplementation(useStateMock);
+    const props = {
+      leftPart: 50,
+      topPart: 50,
+      childrenProps: (
+        <View style={{ borderColor: 'red', borderWidth: 1, width: 167, height: 167 }} />
+      ),
+    };
+    const changeRotate = () => {};
+    const wrapper = shallow(<SteeringWheel {...props} changeRotate={changeRotate} />);
+    wrapper.simulate('layout', {
+      nativeEvent: {
+        layout: { x: 200, y: 277, width: 167, height: 167 },
+      },
+    });
+    const wrapperHandle = wrapper.children().at(0);
+    wrapperHandle.simulate('handlerStateChange', {
+      nativeEvent: {
+        state: State.BEGAN,
+        absoluteX: 144,
+        absoluteY: 295,
+      },
+    });
+    wrapper.update();
+    wrapperHandle.simulate('handlerStateChange', {
+      nativeEvent: {
+        state: State.ACTIVE,
+        absoluteX: 144,
+        absoluteY: 295,
+      },
+    });
+    wrapper.update();
+    wrapperHandle.simulate('gestureEvent', {
+      nativeEvent: {
+        state: State.ACTIVE,
+        x: 888,
+        y: 888,
+      },
+    });
+    wrapper.update();
+
+    wrapperHandle.simulate('gestureEvent', {
+      nativeEvent: {
+        state: State.ACTIVE,
+        x: 110,
+        y: 283,
+      },
+    });
+    wrapperHandle.simulate('gestureEvent', {
+      nativeEvent: {
+        state: State.ACTIVE,
+        x: 263,
+        y: 271,
+      },
+    });
+    wrapperHandle.simulate('gestureEvent', {
+      nativeEvent: {
+        state: State.END,
+        x: 212,
+        y: 222,
+      },
+    });
+    wrapperHandle.simulate('handlerStateChange', {
+      nativeEvent: {
+        state: State.END,
+        absoluteX: 144,
+        absoluteY: 295,
+      },
+    });
+    expect(wrapper).toMatchSnapshot();
+
+    wrapper.unmount();
+  });
+});
