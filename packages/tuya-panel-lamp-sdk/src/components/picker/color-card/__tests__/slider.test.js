7e46e10ef66c20c0058272b79c5e4b40dede68d5
asuka.ma <asuka.ma@tuya.com>
2022-02-17 11:35:05 +0800

    feat: add lamp sleep formatter

--- packages/tuya-panel-lamp-sdk/src/components/picker/color-card/__tests__/slider.test.js
+++ packages/tuya-panel-lamp-sdk/src/components/picker/color-card/__tests__/slider.test.js
@@ -22,76 +22,8 @@ describe('Slider', () => {
     wrapper.unmount();
   });
 
-  it('render touch', () => {
-    const wrapper = shallow(<Slider value={100} />);
-    const pander = wrapper.findWhere(
-      c => c.name() === 'View' && !!c.prop('onStartShouldSetResponder') === true
-    );
-
-    pander.simulate('layout', { nativeEvent: { layout: {} } });
-    pander.simulate('startShouldSetResponder', { nativeEvent: { locationX: 45, locationY: 3 } });
-
-    pander.simulate('moveShouldSetResponder', { nativeEvent: { locationX: 45, locationY: 3 } });
-    pander.simulate('responderGrant', {
-      nativeEvent: { touches: [] },
-      touchHistory: { touchBank: [] },
-    });
-    pander.simulate('responderMove', {
-      nativeEvent: { touches: [] },
-      touchHistory: { touchBank: [] },
-    });
-    pander.simulate('responderRelease', {
-      nativeEvent: { touches: [] },
-      touchHistory: { touchBank: [] },
-    });
-
-    pander.simulate('responderTerminationRequest');
-    pander.simulate('responderTerminate');
-    expect(wrapper).toMatchSnapshot();
-  });
-
-  it('render touch disabled', () => {
-    const wrapper = shallow(<Slider value={100} disabled />);
-    const pander = wrapper.findWhere(
-      c => c.name() === 'View' && !!c.prop('onStartShouldSetResponder') === true
-    );
-
-    pander.simulate('startShouldSetResponder', { nativeEvent: { locationX: 45, locationY: 3 } });
-
-    pander.simulate('moveShouldSetResponder');
-    pander.simulate('responderGrant', {
-      nativeEvent: { touches: [] },
-      touchHistory: { touchBank: [] },
-    });
-    pander.simulate('responderMove', {
-      nativeEvent: { touches: [] },
-      touchHistory: { touchBank: [] },
-    });
-    pander.simulate('responderRelease', {
-      nativeEvent: { touches: [] },
-      touchHistory: { touchBank: [] },
-    });
-
-    pander.simulate('responderTerminationRequest');
-    pander.simulate('responderTerminate');
-    expect(wrapper).toMatchSnapshot();
-  });
-
   it('basic mount', () => {
     const wrapper = mount(<Slider value={100} />);
-    expect(wrapper).toMatchSnapshot();
-
-    wrapper.unmount();
-  });
-
-  it('Percent render', () => {
-    const wrapper = shallow(<Percent percent={10} />);
-
-    wrapper.setProps({ percent: 30 });
-    wrapper.setProps({ percent: 70 });
-
-    wrapper.instance().setNativeProps({ percent: 50 });
-
     expect(wrapper).toMatchSnapshot();
 
     wrapper.unmount();
